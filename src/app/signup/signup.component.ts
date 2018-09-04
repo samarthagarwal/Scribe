42c9c2f4ad7717d05b019cbbf362797b16162405
unknown <samarthagarwal@live.com>
2018-09-05 00:12:55 +0530

    Custom Validations

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -18,8 +18,26 @@ export class SignupComponent implements OnInit {
       email: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: this.checkIfMatchingPasswords("password", "confirmPassword")
     })
 
+  }
+
+  checkIfMatchingPasswords(passwordKey: string, confirmPasswordKey: string){
+    return (group: FormGroup) => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+
+      if(password.value == confirmPassword.value){
+        return;
+      } else {
+        confirmPassword.setErrors({
+          notEqualToPassword: true
+        })
+      }
+
+    }
   }
 
   onSubmit(signupform){

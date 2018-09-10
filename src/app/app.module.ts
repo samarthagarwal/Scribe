b657f4c9e414d0bb7296a544e6cc1c4f63083449
unknown <samarthagarwal@live.com>
2018-09-11 00:09:53 +0530

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+
+let config = {
+  apiKey: "AIzaSyDyT_uO9pFs3nDvJWDGH78z14Rkz8G-Je4",
+  authDomain: "scribe-d63dd.firebaseapp.com",
+  databaseURL: "https://scribe-d63dd.firebaseio.com",
+  projectId: "scribe-d63dd",
+  storageBucket: "scribe-d63dd.appspot.com",
+  messagingSenderId: "236969302096"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [

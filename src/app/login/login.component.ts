3b07644219e77cca479b0f4fe0cb57f58894da5a
unknown <samarthagarwal@live.com>
2018-09-15 23:17:45 +0530

    Login Completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  message: string = "";
+  userError: any;
+
+  constructor(public fb: FormBuilder) {
+
+    this.myForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]]
+    })
+
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(form){
+
+    firebase.auth().signInWithEmailAndPassword(form.value.email, form.value.password).then((data) => {
+      console.log(data);
+      this.message = "You have been logged in successfully."
+    }).catch((error) => {
+      console.log(error);
+      this.userError = error;
+    })
+
   }
 
 }

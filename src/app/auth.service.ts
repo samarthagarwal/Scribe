bfbe3eacbb832ca2c4cb53725c1f843125b6ecb7
unknown <samarthagarwal@live.com>
2018-09-25 15:59:32 +0530

    AuthService created

---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  login(email: string, password: string){
+
+  }
+
+  signup(email: string, password: string, first_name: string, last_name: string){
+    
+  }
+
+}

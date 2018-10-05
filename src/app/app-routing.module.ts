377180537a5f79167bfce738d19265a605007294
unknown <samarthagarwal@live.com>
2018-10-05 14:01:16 +0530

    Added Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  path: '', component: HomeComponent
+},{
+  path: 'login', component: LoginComponent
+}]
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

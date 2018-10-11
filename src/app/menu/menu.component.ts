3f4f8c1f00fd4a7c3237c6bf51e3c01523382e08
unknown <samarthagarwal@live.com>
2018-10-12 00:15:43 +0530

    Finalized menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-menu',
@@ -8,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   loggedIn: boolean = false;
-  constructor() { }
+  user: any;
+
+  constructor() {
+
+    this.user = firebase.auth().currentUser;
+
+    if(this.user) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+
+    firebase.auth().onAuthStateChanged((user) => {
+
+      if(user){
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+
+    })
+
+  }
 
   ngOnInit() {
+  }
+
+  logout(){
+    firebase.auth().signOut();
   }
 
 }

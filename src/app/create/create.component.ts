1392f86680bb1870a3bdf576f8add6d5ad54fc5a
unknown <samarthagarwal@live.com>
2018-10-16 01:16:15 +0530

    createPost() done

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  editorConfig: any;
+  title: string;
+  content: string;
+
+  constructor() {
+    this.editorConfig = {
+      "editable": true,
+      "spellcheck": true,
+      "height": "auto",
+      "minHeight": "150px",
+      "width": "auto",
+      "minWidth": "0",
+      "translate": "yes",
+      "enableToolbar": true,
+      "showToolbar": false,
+      "placeholder": "Enter text here...",
+      "imageEndPoint": "",
+      "toolbar": [
+        ["bold", "italic", "underline", "strikeThrough", "superscript", "subscript"],
+        ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent", "outdent"],
+        ["cut", "copy", "delete", "removeFormat", "undo", "redo"],
+        ["paragraph", "blockquote", "removeBlockquote", "horizontalLine", "orderedList", "unorderedList"],
+        ["link", "unlink"/*, "image"*/],
+        ["code"]
+      ]
+    }
+  }
 
   ngOnInit() {
+  }
+
+  createPost(){
+
+    firebase.firestore().settings({
+      timestampsInSnapshots: true
+    });
+
+    firebase.firestore().collection("posts").add({
+      title: this.title,
+      content: this.content,
+      owner: firebase.auth().currentUser.uid,
+      created: firebase.firestore.FieldValue.serverTimestamp()
+    }).then((data) => {
+      console.log(data);
+    }).catch((error) => {
+      console.log(error);
+    })
+
   }
 
 }

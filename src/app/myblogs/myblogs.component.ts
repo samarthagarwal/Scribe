327f6265ad63578ba02c2c1761735b8a19a8881e
unknown <samarthagarwal@live.com>
2018-10-16 23:56:43 +0530

    Getting posts

--- src/app/myblogs/myblogs.component.ts
+++ src/app/myblogs/myblogs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-myblogs',
@@ -10,12 +11,38 @@ import 'firebase/auth';
 export class MyblogsComponent implements OnInit {
 
   user: any = {};
+  posts: any[] = [];
 
   constructor() {
+    firebase.firestore().settings({
+      timestampsInSnapshots: true
+    });
     this.user = firebase.auth().currentUser;
+    this.getPosts();
   }
 
   ngOnInit() {
+  }
+
+  getPosts(){
+    // get the list of posts
+
+    firebase.firestore().collection("posts").get().then((querySnapshot) => {
+
+      console.log(querySnapshot.docs);
+      this.posts = querySnapshot.docs;
+
+    }).catch((err) => {
+      console.log(err);
+    })
+
+  }
+
+  onPostCreated(){
+    // refresh the list of posts
+    this.posts = [];
+    this.getPosts();
+
   }
 
 }
